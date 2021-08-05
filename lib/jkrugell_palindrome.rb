52d521be3dd746639604bf1aacdf19d68c5aa5f6
johannkrugell <johannkrugell@gmail.com>
2021-08-06 07:07:08 +1000

    Add test for integer palindrome

--- lib/jkrugell_palindrome.rb
+++ lib/jkrugell_palindrome.rb
@@ -2,22 +2,31 @@
 
 require_relative "jkrugell_palindrome/version"
 
+module JkrugellPalindrome
+
+# Returns true for a palindrome, false otherwise.
+def palindrome?
+  processed_content == processed_content.reverse
+end
+
+def letters
+  # self.chars.select { |c| c.match(/[a-z]/i) }.join
+  self.scan(/[a-z\d]/i).join
+end
+
+private
+
+  # Returns content for palindrome testing.
+  def processed_content
+    self.to_s.letters.downcase
+  end
+  
+end
+
 class String
+  include JkrugellPalindrome
+end
 
-  # Returns true for a palindrome, false otherwise.
-  def palindrome?
-    processed_content == processed_content.reverse
-  end
-
-  def letters
-    # self.chars.select { |c| c.match(/[a-z]/i) }.join
-    self.scan(/[a-z]/i).join
-  end
-
-  private
-
-    # Returns content for palindrome testing.
-    def processed_content
-      self.letters.downcase
-    end
+class Integer
+  include JkrugellPalindrome
 end
